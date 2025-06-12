4ca111d9b10dad11bcad0fd28f562da5f70b60b9
Matias <lit.io30303@gmail.com>
2025-06-12 11:05:45 -0400

    fix mapa

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -3,6 +3,9 @@
 import { Transition } from "../Transition";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
+import { Map } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const MapContainer = dynamic(
   () => import("react-leaflet").then((mod) => mod.MapContainer),
@@ -12,19 +15,16 @@ const TileLayer = dynamic(
   () => import("react-leaflet").then((mod) => mod.TileLayer),
   { ssr: false }
 );
+
 const MarkerHouse = dynamic(
   () => import("./MarkerHouse").then((mod) => mod.MarkerHouse),
   { ssr: false }
 );
+
+
 export function Location() {
   const coordinatePoint = { lat: 40.463667, lng: -3.74922 };
-  const [leaflet, setLeaflet] = useState<typeof import("leaflet") | null>(null);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      require("leaflet/dist/leaflet.css"); // ✅ Alternativa con require()
-    }
-  }, []);
 
   const centerMarker = (position: { lat: number; lng: number }, fnMap: any) => {
     if (fnMap?.flyTo) {
@@ -40,17 +40,15 @@ export function Location() {
       <h2 className="max-w-2xl mx-auto my-4 mb-8 text-3xl font-semibold text-center">
         Disponibles en todo Puerto Varas
       </h2>
-      {leaflet && (
-        <MapContainer
-          center={coordinatePoint}
-          zoom={6}
-          scrollWheelZoom={false}
-          className="h-[700px]"
-        >
-          <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png" />
-          <MarkerHouse selectMarker={centerMarker} />
-        </MapContainer>
-      )}
+      <MapContainer
+        center={coordinatePoint}
+        zoom={6}
+        scrollWheelZoom={false}
+        className="h-[700px]"
+      >
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png" />
+        <MarkerHouse selectMarker={centerMarker} />
+      </MapContainer>
     </Transition>
   );
 }
