79ad611224c60134d9efab6c4599c4bff8161321
Matias <lit.io30303@gmail.com>
2025-08-19 20:31:21 -0400

    # add buton wsp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { CiMenuFries } from "react-icons/ci";
 import { BsTelephone } from "react-icons/bs";
 import { useState } from "react";
 import { Navbar } from "..";
+import { FaWhatsapp } from "react-icons/fa";
 
 export function Header() {
   const [openMobileMenu, setOpenMobileMenu] = useState(false);
@@ -20,15 +21,20 @@ export function Header() {
           onClick={() => setOpenMobileMenu(!openMobileMenu)}
         ></CiMenuFries>
         <Navbar openMobileMenu={openMobileMenu}></Navbar>
-        
+
         <div className="flex items-center gap-2 md:gap-5">
-          <Link
-            href={"tel:99479312"}
-            className="flex items-center gap-4 cursor-pointer"
+          <a
+            href="https://wa.me/56999479312?text=Hola!%20Estoy%20interesado%20en%20los%20arriendos%20de%20Puerto%20Varas.%20¿Me%20puedes%20contar%20más?"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-4 cursor-pointer text-green-600 hover:text-green-800"
           >
-            <BsTelephone></BsTelephone>
-            <span className="hidden md:block">+569 99479312</span>
-          </Link>
+            <div className="bg-green-500 hover:bg-green-600 text-white px-3 py-2 rounded-lg flex items-center gap-2">
+              <FaWhatsapp size={18} />
+              <span className="hidden md:block">Contáctanos</span>
+            </div>
+          </a>
+
           {/* <Link href={"/login"} className="px-3 py-2 text-white rounded-lg bg-secondary hover:bg-black">Login</Link> */}
         </div>
       </div>
